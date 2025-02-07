f6bfe3b8ceb579a0d8494401a7d40da0ffb7e547
TatianaChF <tatiana.chefox@gmail.com>
2025-02-07 21:12:04 +0300

    add render to onMounted

--- src/components/FiguresComponent.vue
+++ src/components/FiguresComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import * as THREE from "three";
-  import {ref} from "vue";
+  import {onMounted, ref} from "vue";
 
   const element = ref<HTMLCanvasElement | null>(null);
   const scene = new THREE.Scene();
@@ -12,12 +12,14 @@
   );
   scene.add(camera);
 
-  const renderer = new THREE.WebGLRenderer({
-    canvas: element.value as unknown as HTMLCanvasElement,
-    antialias: true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.render(scene, camera);
+  onMounted(() => {
+    const renderer = new THREE.WebGLRenderer({
+      canvas: element.value as unknown as HTMLCanvasElement,
+      antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  })
 </script>
 
 <template>
