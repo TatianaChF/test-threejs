410f47e55f7535b09f2d75dbce699e4f11557488
TatianaChF <tatiana.chefox@gmail.com>
2025-02-07 22:09:49 +0300

    update format

--- src/components/FiguresComponent.vue
+++ src/components/FiguresComponent.vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
-  import * as THREE from "three";
-  import {onMounted, ref} from "vue";
+import * as THREE from "three";
+import {onMounted, ref} from "vue";
 
-  const element = ref<HTMLCanvasElement | null>(null);
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-  );
-  camera.position.z = 5;
-  scene.add(camera);
+const element = ref<HTMLCanvasElement | null>(null);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 5;
+scene.add(camera);
 
-  const sphere = new THREE.Mesh(
-      new THREE.SphereGeometry(1, 20, 20),
-      new THREE.MeshBasicMaterial({color: "red"}),
-  );
-  sphere.position.set(-2,0,0);
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 20, 20),
+    new THREE.MeshBasicMaterial({color: "red"}),
+);
+sphere.position.set(-2,0,0);
 
-  const cube = new THREE.Mesh(
-      new THREE.BoxGeometry(1,1,1,),
-      new THREE.MeshBasicMaterial({color: "purple"}),
-  );
-  cube.position.set(0,0,0);
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1,),
+    new THREE.MeshBasicMaterial({color: "purple"}),
+);
+cube.position.set(0,0,0);
 
-  const group = new THREE.Group();
-  group.add(sphere, cube);
-  scene.add(group);
+const group = new THREE.Group();
+group.add(sphere, cube);
+scene.add(group);
 
-  onMounted(() => {
-    const renderer = new THREE.WebGLRenderer({
-      canvas: element.value as unknown as HTMLCanvasElement,
-      antialias: true
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
-  })
+onMounted(() => {
+  const renderer = new THREE.WebGLRenderer({
+    canvas: element.value as unknown as HTMLCanvasElement,
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+})
 </script>
 
 <template>
