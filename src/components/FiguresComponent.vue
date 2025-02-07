896e2ec9f89f121f804d3917fc740e410de3f1a0
TatianaChF <tatiana.chefox@gmail.com>
2025-02-07 20:50:30 +0300

    add scene and camera

---
+++ src/components/FiguresComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import * as THREE from "three";
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+  );
+  scene.add(camera);
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
