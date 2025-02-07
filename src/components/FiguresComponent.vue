b2bd421b3342c5c2ec2bf1d61211ad86eb251277
TatianaChF <tatiana.chefox@gmail.com>
2025-02-07 21:10:38 +0300

    add render and canvas

--- src/components/FiguresComponent.vue
+++ src/components/FiguresComponent.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
   import * as THREE from "three";
+  import {ref} from "vue";
 
+  const element = ref<HTMLCanvasElement | null>(null);
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
       75,
@@ -9,10 +11,17 @@
       1000
   );
   scene.add(camera);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: element.value as unknown as HTMLCanvasElement,
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 </script>
 
 <template>
-
+  <canvas ref="element"/>
 </template>
 
 <style scoped>
